f6a2bbb0924afa885765079938f1a349ca4f1d6b
lukaseder <lukas.eder@gmail.com>
2015-07-29 18:07:21 +0200

    Fix whitespace

--- jdbc/index.js
+++ jdbc/index.js
@@ -8,29 +8,29 @@ var Properties = Java.type("java.util.Properties")
   ;
  
 try {
-	properties.setProperty("user", "sa");
+    properties.setProperty("user", "sa");
     properties.setProperty("password", "");
-	
-	connection = driver.connect("jdbc:h2:~/nasven-jdbc", properties);
-	stmt = connection.prepareStatement("SELECT * FROM information_schema.tables WHERE table_name LIKE ?");
-	stmt.setString(1, '%COLUMN%');
-	rs = stmt.executeQuery();
-	
-	print("\n");
-	print("INFORMATION_SCHEMA Tables matching %COLUMN%");
-	print("-------------------------------------------");
-	while (rs.next()) {
-		print(rs.getString("TABLE_NAME"));
-	}
-	print("\n");
+    
+    connection = driver.connect("jdbc:h2:~/nasven-jdbc", properties);
+    stmt = connection.prepareStatement("SELECT * FROM information_schema.tables WHERE table_name LIKE ?");
+    stmt.setString(1, '%COLUMN%');
+    rs = stmt.executeQuery();
+    
+    print("\n");
+    print("INFORMATION_SCHEMA Tables matching %COLUMN%");
+    print("-------------------------------------------");
+    while (rs.next()) {
+        print(rs.getString("TABLE_NAME"));
+    }
+    print("\n");
 }
 finally {
-	if (rs)
-		rs.close();
-	
-	if (stmt)
-		stmt.close();
-	
-	if (connection)
-		connection.close();
+    if (rs)
+        rs.close();
+    
+    if (stmt)
+        stmt.close();
+    
+    if (connection)
+        connection.close();
 }
