d60bed0043b516ea34b986d9c50692a8f77e20ec
Bruno Borges <bruno.borges@gmail.com>
2015-11-03 04:15:06 -0800

    proper jaxrs code

--- jaxrs/index.js
+++ jaxrs/index.js
@@ -8,8 +8,9 @@
 var ClientBuilder = Packages.javax.ws.rs.client.ClientBuilder;
 var client = ClientBuilder.newClient();
 var target = client.target("http://jsonplaceholder.typicode.com/users");
-var response = JSON.parse(target.request().get(java.lang.String.class));
-var length = response.length;
-print("Number of users is ${response.length}");
+var response = target.request().get(java.lang.String.class);
+var json = JSON.parse(response);
+var length = json.length;
+print("Number of users is ${json.length}");
 
-print(JSON.stringify(response, null, 4));
+print(JSON.stringify(json, null, 4));
